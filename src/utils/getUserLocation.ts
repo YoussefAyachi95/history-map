3f9a944e5df9229151375881bca0c0751f28d30c
Youssef <youssef1995elam@gmail.com>
2024-01-09 16:15:58 +0100

    feature: Add get user location

---
+++ src/utils/getUserLocation.ts
@@ -0,0 +1,19 @@
+export const getUserLocation = (): Promise<[number, number] | null> => {
+    return new Promise((resolve, reject) => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            resolve([latitude, longitude]);
+          },
+          (error) => {
+            console.error('Error getting user location:', error);
+            reject(null); 
+          }
+        );
+      } else {
+        console.error('Geolocation is not supported');
+        reject(null);
+      }
+    });
+  };
