09247b830251d54a9161e32f3a01495452e95045
Youssef <youssef1995elam@gmail.com>
2024-01-15 15:31:21 +0100

    Fix: City placeholder

--- src/components/Location.tsx
+++ src/components/Location.tsx
@@ -3,12 +3,13 @@ import Image from 'next/image';
 
 
 export default function Location({ onClick, city }: LocationProps) {
+    
   return (
     <button
       className="p-2 rounded-md cursor-pointer flex items-center justify-center gap-2 text-white font-semibold"
       onClick={onClick}
     >
-        {city}
+        {city ? city : "London"}
       <Image src={"location.svg"} width={30} height={30} alt="Location Icon" /> 
     </button>
   );
